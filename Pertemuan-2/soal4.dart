126cf78b22e241469c825962df25538b59d8edca
agussalim63663-tech <agussalim63663@gmail.com>
2026-02-26 14:20:47 +0800

    Add files via upload

---
+++ Pertemuan-2/soal4.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  String namaLengkap = "agussalim";
+  String nim = "H0724517";
+  double ipk = 3.68;
+  int semester = 4;
+  bool statusBeasiswa = true;
+
+  print("Nama Lengkap       : $namaLengkap");
+  print("NIM                : $nim");
+  print("IPK                : $ipk");
+  print("Semester saat ini  : $semester");
+  print("Status Beasiswa    : ${statusBeasiswa}");
+}
